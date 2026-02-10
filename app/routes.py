f586e1a30772290467ab87813aa426cf2343f132
johnclamy <johnclamy@gmail.com>
2026-02-10 18:09:55 +0000

    return a string of html in routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,17 @@
 from app import app
 
+
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Jonathan'}
+    return '''
+    <html>
+    <head>
+        <title>Microblog | Home page</title>
+    </head>
+    <body>
+        <h1>Hello, {}!</h1>
+    </body>
+    </html>
+'''.format(user['username'])
