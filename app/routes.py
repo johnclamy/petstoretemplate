1967ac77fe7a039bf6e3f6621cf8acff756d2a12
johnclamy <johnclamy@gmail.com>
2026-02-11 11:04:01 +0000

    add dummy posts for testing

--- app/routes.py
+++ app/routes.py
@@ -6,4 +6,15 @@ from flask import render_template
 @app.route('/index')
 def index():
     user = { 'username': 'Jonathan'}
-    return render_template('index.html', title='Home Page', user=user)
+    posts = [
+        {
+            'author': {'username': 'Molly'},
+            'body': 'React.js and Flask are so cool together!'
+        },
+        {
+            'author': {'username': 'Amir'},
+            'body': 'I love Python and JavaScript!'
+        }
+    ]
+
+    return render_template('index.html', title='Home Page', user=user, posts=posts)
